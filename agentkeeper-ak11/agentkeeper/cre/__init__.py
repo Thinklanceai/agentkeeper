5aeedbdc9957050d3e2f9115dc5ea28d7c886bca
tomanciauxberner-rgb <tom.anciauxberner@gmail.com>
2026-05-20 00:39:32 +0200

    chore: remove stale agentkeeper-ak11/ sprint workspace from repo

--- agentkeeper-ak11/agentkeeper/cre/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Cognitive Reconstruction Engine module."""
-
-from .engine import CognitiveReconstructionEngine, estimate_tokens
-
-__all__ = ["CognitiveReconstructionEngine", "estimate_tokens"]
--- agentkeeper-ak11/agentkeeper/storage/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Persistence backends."""
-
-from .sqlite_store import Storage
-
-__all__ = ["Storage"]
